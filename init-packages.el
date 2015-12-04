8583e7ef78e6ce1cd123d575d06a3a81027044f2
gniuk <isgniuk@gmail.com>
2015-12-04 11:02:21 +0800

    reactivate helm-gtags, used when writing code. After build use rtags.

--- init-packages.el
+++ init-packages.el
@@ -148,15 +148,20 @@
 (add-hook 'c++-mode-hook 'helm-gtags-mode)
 (add-hook 'asm-mode-hook 'helm-gtags-mode)
 
-;; (eval-after-load "helm-gtags"
-;;       '(progn
-;;          (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-find-tag)
-;;          (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
-;;          (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
-;;          (define-key helm-gtags-mode-map (kbd "M-g M-p") 'helm-gtags-parse-file)
-;;          (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-;;          (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
-;;          (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)))
+;; (custom-set-variables
+;;  '(helm-gtags-path-style 'relative)
+;;  '(helm-gtags-ignore-case t)
+;;  '(helm-gtags-auto-update t))
+
+(eval-after-load "helm-gtags"
+      '(progn
+         (define-key helm-gtags-mode-map (kbd "C-c g t") 'helm-gtags-find-tag)
+         (define-key helm-gtags-mode-map (kbd "C-c g r") 'helm-gtags-find-rtag)
+         (define-key helm-gtags-mode-map (kbd "C-c g s") 'helm-gtags-find-symbol)
+         (define-key helm-gtags-mode-map (kbd "C-c g p") 'helm-gtags-parse-file)
+         (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+         (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+         (define-key helm-gtags-mode-map (kbd "C-c g ,") 'helm-gtags-pop-stack)))
 
 ;;; smartscan
 ; smart scan replace text! M-' good!
