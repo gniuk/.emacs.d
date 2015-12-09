8feecf3178b62ddaaa6eb8905417abf0ddb59059
gniuk <isgniuk@gmail.com>
2015-12-09 21:23:45 +0800

    function used to clear eshell buffer by type execute clear in eshell.

--- mylisp/useful-single-key.el
+++ mylisp/useful-single-key.el
@@ -41,6 +41,13 @@
 ; in current line.
 (global-set-key (kbd "C-o") 'gniuk/insert-line-below)
 
+;; clear eshell buffer, by type clear and execute
+(defun eshell/clear ()
+  "Clear the eshell buffer."
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (eshell-send-input)))
+
 (provide 'useful-single-key)
 
 ;; Local Variables:
