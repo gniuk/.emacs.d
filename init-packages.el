e09ee1e80d892bbe92f2620dc881f96225c07ea5
gniuk <gniuk@sina.com>
2019-05-31 15:52:57 +0800

    add godef for jump to definition and jump back

--- init-packages.el
+++ init-packages.el
@@ -663,6 +663,15 @@
 ;;    ((((type x)) (:inherit company-tooltip-selection :weight bold))
 ;;     (t (:inherit company-tooltip-selection)))))
 
+;;; go mode
+(defun my-go-mode-hook ()
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-,") 'pop-tag-mark))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
 ;;; go-eldoc
 ;(require 'go-eldoc) ;; Don't need to require, if you install by package.el
 (add-hook 'go-mode-hook 'go-eldoc-setup)
