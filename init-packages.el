3b251f1ef5653dfb16fccefe069b91a99c763f51
gniuk <gniuk@sina.com>
2020-01-09 16:39:42 +0800

    no rtags-autostart-diagnostics

--- init-packages.el
+++ init-packages.el
@@ -341,7 +341,7 @@
 (eval-after-load 'company
   '(add-to-list
     'company-backends 'company-rtags))
-(setq rtags-autostart-diagnostics t)
+;(setq rtags-autostart-diagnostics t)
 (rtags-enable-standard-keybindings)
 (require 'helm-rtags)
 (setq rtags-use-helm t)
