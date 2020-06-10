a073f750b11231bd82db642cdc8951e6a4477f3b
gniuk <isgniuk@gmail.com>
2020-06-11 02:55:03 +0800

    Eliminate warnings and replace cl with cl-lib.

--- packages.el
+++ packages.el
@@ -1,4 +1,12 @@
-; packages.el
+;;; packages.el --- Melpa packages -*- lexical-binding: t; coding: utf-8 -*-
+
+;; Copyright (C) 2013-2020 gniuk <isgniuk@gmail.com>
+
+;;; Commentary:
+;; none
+
+;;; Code:
+
 ;; (require 'package)
 ;; (add-to-list 'package-archives
 ;;              '("melpa" . "http://melpa.milkbox.net/packages/") t)
@@ -150,13 +158,13 @@ There are two things you can do about this warning:
     ;; benchmark-init
     ) "A list of packages to ensure are installed at launch.")
 
-(require 'cl)
+(require 'cl-lib)
 
-; method to check if all packages are installed
-(defun packages-installed-p ()
-  (loop for p in required-packages
-        when (not (package-installed-p p)) do (return nil)
-        finally (return t)))
+(cl-defun packages-installed-p ()
+  "Method to check if all packages are installed."
+  (cl-loop for p in required-packages
+        when (not (package-installed-p p)) do (cl-return nil)
+        finally (cl-return t)))
 
 ; if not all packages are installed, check one by one and install the missing ones.
 (unless (packages-installed-p)
@@ -165,6 +173,10 @@ There are two things you can do about this warning:
   (package-refresh-contents)
   (message "%s" " done.")
   ; install the missing packages
-  (dolist (p required-packages)
+  (cl-dolist (p required-packages)
     (when (not (package-installed-p p))
       (package-install p))))
+
+(provide 'packages)
+
+;;; packages.el ends here
